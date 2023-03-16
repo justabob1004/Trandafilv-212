2eddfdec00e2df84fc6650ad6a0864e8b1c9ffb5
justabob1004 <124868780+justabob1004@users.noreply.github.com>
2023-03-16 12:15:36 +0200

    .

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _16._03._23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 8, -1, 4, 16, -5, 3 }; //Нахождение максимального элемента и его индекса.
+            int max = array[0]; 
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] > max)
+                {
+                    max = array[i];
+                }
+
+            }
+            Console.WriteLine($"max={max}");
+        }
+    }
+}
